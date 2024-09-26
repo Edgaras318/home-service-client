d1c0a9b53dd0fbad85c17f9f556b40317bbf6297
edgaras.spiridonovas <edgaras.spiridonovas@fltechnics.com>
2024-09-26 18:55:43 +0300

    Simplify Topbar and abstract user avatar
    Moved user avatar logic and styles to a new UserAvatar component for better separation of concerns. This refactor improves code readability and maintainability by isolating specific functionality into distinct components. Updated Topbar to utilize the new UserAvatar component.

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,4 +1,3 @@
-// src/components/Topbar/Topbar.js
 import React, { useState, useEffect } from 'react';
 import styles from './Topbar.module.scss';
 import logo from '@/assets/logo.svg';
@@ -6,11 +5,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import Button from "@/components/common/Button/Button";
 import { useUserStore } from '@/stores/userStore';
 import routes from '@/routes'; // Import the routes configuration
+import UserAvatar from '@/components/UserAvatar/UserAvatar'; // Import the new UserAvatar component
 
 const Topbar = () => {
   const { user, clearUser } = useUserStore();
   const navigate = useNavigate();
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const navLinks = [
     { href: routes.home, label: 'See our services', text: 'Home' },
@@ -25,37 +24,11 @@ const Topbar = () => {
   const handleLogout = () => {
     clearUser();
     navigate(routes.home); // Redirect to home after logout
-    setIsDropdownOpen(false); // Close dropdown on logout
-  };
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
   };
 
   useEffect(() => {
-    if (user) {
-      setIsDropdownOpen(false); // Close dropdown when user logs in
-    }
+    // No need to handle dropdown here anymore since it's moved to UserAvatar
   }, [user]);
-
-  const renderUserAvatar = () => {
-    if (user && user.username) {
-      const firstLetter = user.username.charAt(0).toUpperCase();
-      return (
-          <div className={styles.avatar} onClick={toggleDropdown} aria-label="User Menu">
-            {firstLetter}
-            {isDropdownOpen && (
-                <div className={styles.dropdown}>
-                  <button onClick={handleLogout} className={styles.logoutButton}>
-                    Logout
-                  </button>
-                </div>
-            )}
-          </div>
-      );
-    }
-    return null;
-  };
 
   return (
       <div className={styles.topbar}>
@@ -79,7 +52,7 @@ const Topbar = () => {
           </nav>
         </div>
         {user ? (
-            renderUserAvatar()
+            <UserAvatar username={user.username} onLogout={handleLogout} />
         ) : (
             <Button size="large" text="Login / Sign Up" onClick={handleButtonClick} />
         )}
